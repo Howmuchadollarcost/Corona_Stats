397b12117951002fd0dacb6e244c8b685c8d4fa5
Howmuchadollarcost <muluyonas@yahoo.com>
2020-03-17 18:13:47 +0100

    Updates

---
+++ precache-manifest.cfd4bf66c8c497f8d62d23300d1f0c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675e626e064a57cd816fceb6961cc60c",
+    "url": "/Corona_Stats/index.html"
+  },
+  {
+    "revision": "456218a8a0251ae6f132",
+    "url": "/Corona_Stats/static/css/main.b6ae0679.chunk.css"
+  },
+  {
+    "revision": "cf2612619c57216ef232",
+    "url": "/Corona_Stats/static/js/2.df10cf1f.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Corona_Stats/static/js/2.df10cf1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456218a8a0251ae6f132",
+    "url": "/Corona_Stats/static/js/main.e6730be3.chunk.js"
+  },
+  {
+    "revision": "8b88704b706ec5dba9c3",
+    "url": "/Corona_Stats/static/js/runtime-main.553947e9.js"
+  }
+]);
